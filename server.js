df1a75836834cf342dc4c924358e3a6d8281e4d2
russian-proger <sagitov.2806@mail.ru>
2021-07-06 23:20:01 +0500

    server created

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
+const fs   = require('fs');
 const http = require('http');
+
+const express = require('express');
+
+const app = express();
+
+app.use('/public', express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '\\index.html');
+});
+
+const server = http.createServer(app);
+server.listen(80);
