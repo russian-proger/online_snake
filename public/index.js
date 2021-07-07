d4a6065fbf7d5a64f4001a7f449f7048eeb49a01
Russian-Idiot <ranil2086@bk.ru>
2021-07-07 11:04:10 +0600

    canvas was added

--- public/index.js
+++ public/index.js
@@ -1,1 +1,16 @@
-alert("Happy!");
+/**
+ * @type {HTMLCanvasElement}
+ */
+const canvas     = document.getElementById('canvas');
+const context    = canvas.getContext('2d');
+const MAX_WIDTH  = 30; // кол-во клеток ширины
+const MAX_HEIGHT = 30; // кол-во клеток высоты
+
+function update() {
+    
+}
+
+function render() {
+    setTimeout(render, 30);
+}
+render();
